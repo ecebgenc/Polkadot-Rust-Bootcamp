7ad4afea5507ce2d7cb2c7167def0beea5433fdc
Ece Beren GENC <85981166+ecebgenc@users.noreply.github.com>
2023-07-18 18:17:09 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn concatenate_strings(string1: &str, string2: &str) -> String {
+    let mut result = String::new();
+    result.push_str(string1);
+    result.push_str(string2);
+    result
+}
+
 fn main() {
     let string1 = String::from("Hello, all! ");
     // println!("{}", &string1);
@@ -5,11 +12,4 @@ fn main() {
     // println!("{}", &string2);
     let concatenated_string = concatenate_strings(&string1, &string2);
     println!("{}", concatenated_string);
-}
-
-fn concatenate_strings(string1: &str, string2: &str) -> String {
-    let mut result = String::new();
-    result.push_str(string1);
-    result.push_str(string2);
-    result
 }
